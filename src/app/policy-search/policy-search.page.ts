5d8b57dfe453347b2f970db10ce3b8e6e08e8feb
Anukoon <anukoon.ae@gmail.com>
2020-06-21 00:26:29 +0700

    update togle button

--- src/app/policy-search/policy-search.page.ts
+++ src/app/policy-search/policy-search.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NavController } from "@ionic/angular";
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "app-policy-search",
@@ -8,15 +8,41 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ["./policy-search.page.scss"],
 })
 export class PolicySearchPage implements OnInit {
+  option_1_1: boolean = true;
+  option_1_2: boolean = true;
+  option_2_1: boolean = true;
+  option_2_2: boolean = true;
+
   constructor(
     private NavController: NavController,
-    private translateService:TranslateService
-    ) {}
+    private translateService: TranslateService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   searchPolicy() {
     this.NavController.navigateForward(["policy-search-result"]);
+  }
+
+  option_1_Click(option, question) {
+    //toggel optopn 1 ลดหย่อนภาษี
+    if (question === "1") {
+      if (option === "not") {
+        this.option_1_2 = !this.option_1_2;
+      }
+      if (option === "have") {
+        this.option_1_1 = !this.option_1_1;
+      }
+    }
+
+    //toggel optopn 2 ประกันสุขภาพ
+    if (question === "2") {
+      if (option === "not") {
+        this.option_2_2 = !this.option_2_2;
+      }
+      if (option === "have") {
+        this.option_2_1 = !this.option_2_1;
+      }
+    }
   }
 }
